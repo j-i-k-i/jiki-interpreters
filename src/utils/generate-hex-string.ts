e4007c8426dbefac6003ff7a5da8b2da8a686382
Jeremy Walker <jez.walker@gmail.com>
2025-09-04 18:13:14 +0200

    Remove unused files

--- src/utils/generate-hex-string.ts
+++
@@ -1,7 +0,0 @@
-export function generateHexString(length: number): string {
-  let result = "";
-  while (result.length < length) {
-    result += Math.random().toString(16).slice(2);
-  }
-  return result.slice(0, length);
-}
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,7 +1,1 @@
-export { handleNavbarFocus } from "./navbar-focus-handler";
-export { scrollIntoView } from "./scroll-into-view";
-export { redirectTo } from "./redirect-to";
 export { typecheck } from "./typecheck";
-export { showSiteFooter } from "./show-site-footer";
-export { debounce } from "./debounce";
-export { bodyHasClassName } from "./body-has-classname";
